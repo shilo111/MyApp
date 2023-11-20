1b07858be92caf7b977d3a0d0fd24a2819c5ef39
shilo <dadon5609@gmail.com>
2023-11-20 11:58:45 +0200

    d

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -1,20 +1,35 @@
 package com.example.myapplication.ui.dashboard;
 
-import android.os.Bundle;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+
+
+    private EditText editTextFood, editTextCalories;
+    private TextView textViewTotalCalories;
+    private int totalCalories = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,7 +41,19 @@ public class DashboardFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+
+        try {
+            showHomePageDesign(root);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (java.lang.InstantiationException e) {
+            throw new RuntimeException(e);
+        }
         return root;
+
+
     }
 
     @Override
@@ -34,4 +61,35 @@ public class DashboardFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+    private void showHomePageDesign(View view) throws IllegalAccessException, java.lang.InstantiationException {
+
+        editTextFood = view.findViewById(R.id.editTextFood);
+        editTextCalories = view.findViewById(R.id.editTextCalories);
+        textViewTotalCalories = view.findViewById(R.id.textViewTotalCalories);
+        Button btn = view.findViewById(R.id.buttonAdd);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String foodItem = editTextFood.getText().toString();
+                String caloriesStr = editTextCalories.getText().toString();
+
+                if (!foodItem.isEmpty() && !caloriesStr.isEmpty()) {
+                    int calories = Integer.parseInt(caloriesStr);
+                    totalCalories += calories;
+
+                    // Display the updated total calories
+                    textViewTotalCalories.setText("Total Calories: " + totalCalories);
+
+                    // Clear the input fields
+                    editTextFood.getText().clear();
+                    editTextCalories.getText().clear();
+                }
+
+            }
+        });
+    }
+
+
 }
